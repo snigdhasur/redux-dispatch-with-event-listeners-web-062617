56d589cfb532c1b09c794349e230f2570b61cd6d
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-14 14:21:25 -0400

    redux button

--- reducer.js
+++ reducer.js
@@ -7,3 +7,21 @@ function changeState(state = {count: 0}, action){
         return state;
     }
   }
+
+function dispatch(action){
+  state = changeState(state, action);
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({ type: '@@INIT' })
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', function(e) {
+	dispatch({type: 'INCREASE_COUNT'})
+})
